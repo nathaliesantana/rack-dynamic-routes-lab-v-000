38df4a203c3c348ed0d7327c896e704aff1a80c7
Nathalie Santana <nathalie.scruz@gmail.com>
2018-11-14 04:20:27 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,19 @@
 class Application
 
+  @@items= [ ]
+
   def call(env)
 
     if req.path.match(/items/)
-
+      if @@items.include? item
+        resp.write "**price here**"
+      else
+        resp.write "Item not found"
+        resp.status = 404
+      end
     else
 
-      resp.write ""
+      resp.write "Route not found"
       resp.status = 404
     end
 
