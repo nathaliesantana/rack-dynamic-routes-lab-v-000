71340d87016420565ffb4ed72b2cf6fade0732dd
Nathalie Santana <nathalie.scruz@gmail.com>
2018-11-14 04:24:41 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,8 @@ class Application
   @@items= [ ]
 
   def call(env)
+    resp = Rack::Response.new
+    req = Rack::Response.new(env)
 
     if req.path.match(/items/)
       if @@items.include? item
