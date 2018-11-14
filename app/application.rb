14f24d0de7e3595af0e4ffe97f3b168542a4228f
Nathalie Santana <nathalie.scruz@gmail.com>
2018-11-14 04:47:50 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,12 +6,13 @@ class Application
     resp = Rack::Response.new
     req = Rack::Response.new(env)
 
-    if req.path== "/items"
+    if req.path.match(/items/)
+      search_item = req.path.split("/items/").last
       if @@items.include? item
         resp.write "**price here**"
       else
         resp.status = 400
-        resp.write "Item not found"  
+        resp.write "Item not found"
       end
 
     else
