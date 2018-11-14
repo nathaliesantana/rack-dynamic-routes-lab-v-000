484788ae76cda8aff9e067babcee16734b54eff2
Nathalie Santana <nathalie.scruz@gmail.com>
2018-11-14 05:01:27 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+class Application
+
+  @@items= [ ]
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      search_item = req.path.split("/items/").last
+      #binding.pry
+      if item = @@items.find{|s| s.name == search_item}
+        resp.write item.price
+      else
+        resp.status = 400
+        resp.write "Item not found"
+      end
+
+    else
+      resp.status = 404
+      resp.write "Route not found"
+    end
+
+    resp.finish
+
+  end
+
+end
