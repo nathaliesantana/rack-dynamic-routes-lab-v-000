08085010647503f520c482ebef9cf4b68c9fc07f
Nathalie Santana <nathalie.scruz@gmail.com>
2018-11-14 04:38:32 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -13,8 +13,8 @@ class Application
         resp.write "Item not found"
         resp.status = 404
       end
+      
     else
-
       resp.write "Route not found"
       resp.status = 404
     end
